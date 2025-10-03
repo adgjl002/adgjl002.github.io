7321df77df877f1a1e4c415014c4d862a092a25d
SeongYangU <adgjl002@naver.com>
2025-10-03 11:49:28 +0900

    [성양우] 10-03 .deploy 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-# git checkout --orphan gh-pages
+# git checkout --orphan publish
 # git reset --hard
 # echo > .nojekyll   # GitHub의 Jekyll 처리 비활성화(권장)
 # git commit -m "init pages"
-# git push -u origin gh-pages
+# git push -u origin publish
 # git checkout master
 
-git worktree add --force ../_adgjl002_deploy gh-pages
+git worktree add --force ../_adgjl002_deploy publish
 rsync -av --delete site/ ../_adgjl002_deploy
 cd ../_adgjl002_deploy
 touch .nojekyll
